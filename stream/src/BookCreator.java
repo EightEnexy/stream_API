de1edc2a147041992761682ed8c38552bf464f15
EightEnexy <enexyges@gmail.com>
2022-09-19 12:30:57 +0200

    books streams

---
+++ stream/src/BookCreator.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+class BookCreator{
+    public static Book BookCreation() {
+        Random random=new Random();
+
+        String bookName = random
+                            .ints('a','z')
+                            .limit(random.nextInt( 10 - 2) + 2)
+                            .collect(StringBuilder::new, StringBuilder::appendCodePoint,StringBuilder::append)
+                            .toString();
+
+        return new Book(bookName,random.nextInt( 500 - 50) + 50);
+
+
+    }
+}
